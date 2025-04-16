2704fefc1967946478cd896d859177abb90252ee
Silvia Malavasi <smalavasi24@gmail.com>
2025-04-17 00:17:57 +0200

    type

--- types/PokemonCardType.ts
+++ types/PokemonCardType.ts
@@ -1,49 +1,17 @@
-export interface PokemonCard {
-  id: string;
-  name: string;
-  supertype: string;
-  subtypes: string[];
-  hp: string;
-  types: string[];
-  evolvesFrom?: string;
-  flavorText?: string;
-  rarity?: string;
-  image?: string;
-  attacks?: Attack[];
-  abilities?: Ability[];
-  weaknesses?: Weakness[];
-  retreatCost?: string[];
-  convertedRetreatCost?: number;
-  cardSet?: CardSet;
-  number: string;
-  artist?: string;
-  nationalPokedexNumbers?: number[];
-  regulationMark?: string;
-  images?: CardImages;
-}
-
 export interface Ability {
+  id: number;
   name: string;
-  text: string;
   type: string;
+  text: string;
 }
 
 export interface Attack {
+  id: number;
   name: string;
-  cost: string[];
-  convertedEnergyCost: number;
   damage: string;
   text: string;
-}
-
-export interface Weakness {
-  type: string;
-  value: string;
-}
-
-export interface CardImages {
-  small: string;
-  large: string;
+  cost: string[];
+  convertedEnergyCost: number;
 }
 
 export interface CardSet {
@@ -52,13 +20,38 @@ export interface CardSet {
   series: string;
   printedTotal: number;
   total: number;
-  ptcgoCode: string;
   releaseDate: string;
   updatedAt: string;
-  images: SetImages;
-}
-
-export interface SetImages {
   symbol: string;
   logo: string;
+  ptcgoCode: string | null;
+}
+
+export interface PokemonCard {
+  id: string;
+  name: string;
+  supertype: string;
+  subtypes: string[];
+  types: string[];
+  rules: string[] | null;
+  hp: number;
+  evolvesFrom: string | null;
+  evolvesTo: string[] | null;
+  attacks?: Attack[];
+  abilities?: Ability[];
+  weaknesses: { type: string; value: string }[] | null;
+  resistances: { type: string; value: string }[] | null;
+  retreatCost: string[] | null;
+  convertedRetreatCost: number | null;
+  cardSet: CardSet;
+  number: string;
+  artist: string | null;
+  rarity: string | null;
+  flavorText: string | null;
+  nationalPokedexNumbers: number[] | null;
+  regulationMark: string | null;
+  images: {
+    small: string;
+    large: string;
+  };
 }
