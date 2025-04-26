1da75c8ad0da16e088b3855e517c383c1b2ba90d
Silvia Malavasi <smalavasi24@gmail.com>
2025-04-26 02:11:00 +0200

    prima del grosso refactoring con gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Inter: require("../assets/Inter.ttf"),
   });
 
   useEffect(() => {
