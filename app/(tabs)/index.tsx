79a3c822f2761dd2a10242678470a854e7d25986
Silvia Malavasi <smalavasi24@gmail.com>
2025-04-28 16:53:11 +0200

    simplify

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,7 +20,7 @@ export default function HomeScreen() {
         </ThemedText>
         <ThemedView>
           <ThemedButton
-            type="alternative"
+            type="main"
             size="large"
             disabled={false}
             icon="search"
