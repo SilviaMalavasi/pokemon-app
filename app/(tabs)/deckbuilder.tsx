7cbdac8ef382c77f1f836c6f7efae83d4f53a509
Silvia Malavasi <smalavasi24@gmail.com>
2025-05-06 19:30:49 +0200

    testate

--- app/(tabs)/deckbuilder.tsx
+++ app/(tabs)/deckbuilder.tsx
@@ -1,2 +1,53 @@
-import React, { useState } from "react";
-export default function DeckBuilderScreen() {}
+import React from "react";
+import ParallaxScrollView from "@/components/ui/ParallaxScrollView";
+import ThemedText from "@/components/base/ThemedText";
+import ThemedView from "@/components/base/ThemedView";
+import ExternalLink from "@/components/base/ExternalLink";
+import Animated, { useAnimatedRef } from "react-native-reanimated";
+import { useFocusEffect } from "@react-navigation/native";
+
+export default function DeckBuilderScreen() {
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  useFocusEffect(
+    React.useCallback(() => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollTo({ y: 0, animated: true });
+      }
+    }, [])
+  );
+  return (
+    <ParallaxScrollView
+      headerImage="deck-builder-bkg"
+      headerTitle="Pokémon Deck Builder"
+      scrollRef={scrollRef}
+    >
+      <ThemedView>
+        <ThemedText
+          type="defaultSemiBold"
+          style={{ paddingBottom: 12 }}
+        >
+          This app helps players build their Pokémon decks by searching only for cards in the current rotation. LAST
+          UPDATE: 24-04-2025.
+        </ThemedText>
+        <ThemedText
+          type="default"
+          style={{ paddingBottom: 12 }}
+        >
+          This app was created with ♥ by Pokémon nerd and developer{" "}
+          <ExternalLink
+            color="alternative"
+            href="https://www.linkedin.com/in/silvia-malavasi/"
+          >
+            Silvia Malavasi
+          </ExternalLink>{" "}
+          for the Pokémon TCG community. Credits goes to{" "}
+          <ExternalLink href="https://pokemontcg.io/">pokemontcg.io </ExternalLink>
+          for the Card Archive.
+        </ThemedText>
+        <ThemedText type="hintText">
+          This app is not produced, endorsed, supported, or affiliated with Nintendo or The Pokémon Company.
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
